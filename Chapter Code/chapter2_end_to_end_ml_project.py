44d09b3f7945b5b537715df91f43a3971c77f308
Elie Harik <wazaebata@gmail.com>
2019-06-18 08:23:14 -0700

    Start Chap 3

--- Chapter Code/chapter2_end_to_end_ml_project.py
+++
@@ -1,7 +0,0 @@
-## 1. Frame the Problem
-## - What is the business objective?
-##      - How does the company expect to use and benefit from this model?
-## - What does the current solution look like?
-##      - Need a reference for performance and insights on how to solve problem
-## - With current info, ready to design system ->
-##      - Frame the problem: Is it a supervised, unsupervised... ML problem?...
